e59a4e57c315fa36c8b629c8aa5e9c5510c77e67
Suzanna Mitchell <suziequ33@gmail.com>
2023-06-26 16:36:38 -0500

    working on code

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # treehouse-project-5-Public-API-Requests
  treehouse project #5
+/*changed the color of the background to grey. fopund color at computerhope.com*/
--- student-files/js/script.js
+++ student-files/js/script.js
@@ -1,7 +1,6 @@
 const searchContainer = document.querySelector('.search-container');
 const galleryContainer = document.getElementById('gallery');
 const modalContainer = document.createElement('div');
-modalContainer.classList.add('modal-container');
 let users;
 
     const searchMarkup =  `
@@ -12,18 +11,29 @@ let users;
 `;
 searchContainer.insertAdjacentHTML('beforeend', searchMarkup);
 
-//let users = [];
+function fetchData(url) {
+    return fetch(url)
+        .then((response) => {
+            if(!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json()
+        })
+        .then((data) => data.results)
+        .catch((error) => {
+            console.log('Error:', error);
+        });
+}
 fetchData('https://randomuser.me/api/?results=12')
 .then((data) => {
-    users = data.results;
+    users = data;
     users.forEach((user) => {
         createGallyItem(user);
     });
 })
-    .catch((error) => {
-        console.log('Error:', error);
+.catch((error) =>{
+    console.log('Error:', error);
 });
-
 
 function createGallyItem(data) {
     const {picture, name, email, location} = data;
@@ -43,8 +53,11 @@ galleryContainer.insertAdjacentHTML('beforeend', cardMarkup);
 }
 function createModal(data) {
     const {picture, name, email, location, cell, dob} = data;
-    //const formattedCell = formatCellNumber(cell);
-   // const formattedBirthday = formatBirthday(dob.date);
+
+    const formattedCell = formatCellNumber(cell);
+    const formattedAddress = `${location.street.number} ${location.street.name}, ${location.city}, ${location.state}, ${location.postcode}`;
+    const formattedBirthday = formatBirthday(dob.date);
+
 const modalMarkup = `
 <div class="modal-container">
 <div class="modal">
@@ -55,70 +68,52 @@ const modalMarkup = `
             <p class="modal-text">${email}</p>
             <p class="modal-text cap">${location.city}</p>
             <hr>
-            <p class="modal-text">${cell}</p>
-            <p class="modal-text">${location.street.number} ${location.state.name}, ${location.state}, ${location.postcode}</p>
-            <p class="modal-text">Birthday: ${dob.date}</p>
+            <p class="modal-text">${formattedCell}</p>
+            <p class="modal-text">${formattedAddress}</p>
+            <p class="modal-text">Birthday: ${formattedBirthday}</p>
         </div>
  </div>
  </div>
 `;
-//modalContainer.insertAdjacentHTML('beforeend', modalMarkup);
 modalContainer.innerHTML = modalMarkup;
 document.body.appendChild(modalContainer);
-}
 
-galleryContainer.addEventListener('click', (e) => {
-    if (e.target.closest('.card')) {
-        const cardElement = e.target.closest('.card');
-        const userIndex = Array.from(galleryContainer.children).indexOf(e.target.closest('.card'));
-        const user = users[userIndex];
-        createModal(user);
-        //document.body.appendChild(modalContainer);
-    }   
-});
+const closeButton = document.getElementById('modal-close-btn')
+closeButton.addEventListener('click', closeModal);
 
-modalContainer.addEventListener('click', (e) => {
-    if(e.target.id === 'modal-close-btn' || e.target.classList.contains('modal-container')) {
-        //modalContainer.classList.remove();
-        modalContainer.innerHTML = '';
+modalContainer.addEventListener('click', (event) => {
+    if(event.target.classList.contains('modal-container')) {
+        closeModal();
     }
 });
-
-//fetchData('https://randomuser.me/api/?results=12')
-//.then((data) => {
-// users = data.results;
-   // users.forEach((user) => {
-     //   createGallyItem(user);
-   // });
-//})
-   // .catch((error) => {
-      //  console.log('Error:', error);
-//});
-function formatCellNumber(cell) {
-    return cell.replace(/(\d{3})(\d{3})(\d{4})/, '($1) $2-$3');
+modalContainer.classList.add('show-modal');
 }
-
-//function formatCellNumber(cell) {
-   // const cleaned = ('' + cell).replace(/\D/g, '');
-   // const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
-   // if(match) {
-       // return '(' + match[1] + ')' + match[2] + '-' + match[3];
-   // }
-   // return cell;
-//}
+function closeModal() {
+    modalContainer.remove();
+}
+function formatCellNumber(cell) {
+    const cleaned = ('' + cell).replace(/\D/g, '');
+    const match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+    if(match) {
+        return '(' + match[1] + ')' + match[2] + '-' + match[3];
+    }
+    return cell;
+}
 function formatBirthday(birthday) {
     const date = new Date(birthday);
     const month = date.getMonth() + 1;
     const day = date.getDate();
     const year = date.getFullYear();
-    return `${month}/${day}${year}`;
+    return `${month}/${day}/${year}`;
 }
-function fetchData(url) {
-    return fetch(url)
-    .then((response) => {
-        if(response.ok) {
-            return response.json();
-        } 
-           throw new Error('Network response was not ok.');
+
+galleryContainer.addEventListener('click', (event) => {
+    const card = event.target.closest('.card');
+    if(card) {
+        const index = Array.from(galleryContainer.children).indexOf(card);
+        const user = users[index];
+        createModal(user);
+    }
 });
-}
+
+document.body.style.backgroundColor = '#A8A8A8';
